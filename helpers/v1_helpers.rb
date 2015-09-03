c51e9078d15c5ca3b2737e233e39dae31e3ebf7f
Kyrre Havik Eriksen <Kyrremann@gmail.com>
2015-09-03 21:41:41 +0200

    Added more keys to keen

--- helpers/v1_helpers.rb
+++ helpers/v1_helpers.rb
@@ -4,7 +4,9 @@ def retrieve_keen_params(params)
   to_keen[:update] = params["update"]
   to_keen[:language] = params["language"]
   to_keen[:android_version] = params["android_version"]
-  to_keen[:app_version] = params["app_version"]
+  to_keen[:app_version_name] = params["app_version_name"]
+  to_keen[:app_version_code] = params["app_version_code"]
+  to_keen[:country] = params["country"]
   to_keen.delete_if { | k, v | v.nil? }
   to_keen
 end
